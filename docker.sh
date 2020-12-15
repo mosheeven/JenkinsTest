ea7355eae68410a7b1b2c5a86c0335d91c158ffa
Mosheev <mosheeve@gmail.com>
2020-12-15 17:41:56 +0200

    Update docker.sh

--- docker.sh
+++ docker.sh
@@ -1,12 +1,7 @@
 
 node('slave1'){
-    stage('docker-related'){
-        withDockerRegistry(credentialsId: '1cb6f268-6712-4a54-b041-7d7989fa1635') {
-            customImage.push()
-    }
-    }
     stage('deploy'){
-        sh'docker run -t -d --rm devmozes/flaskweb:1.0'
+        sh'docker run -t -d --rm training/webapp:latest'
     }
     
 }
